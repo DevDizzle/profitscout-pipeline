dda3810ed6c466c27ef07ebdb8d25d1decc96801
DevDizzle <eraphaelparra@gmail.com>
2025-10-03 19:14:57 +0000

    feat(ingestion): scaffold prices pipeline package (__init__ only)

---
+++ src/profitscout_ingestion/pipelines/prices/__init__.py
@@ -0,0 +1,9 @@
+"""Prices ingestion pipeline.
+
+This package will contain:
+- extractors: API calls / pagination stubs
+- transformers: normalization & validation stubs
+- loaders: GCS/BQ write stubs
+
+Implementation will be added incrementally to keep diffs small.
+"""
